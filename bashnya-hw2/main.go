a69924dad6912cb820199e65ff5e5c9fb634244c
Машкер <mrndrvsk@mail.ru>
2025-10-18 23:12:31 +0300

    все вот теперь точно конфетка

--- bashnya-hw2/main.go
+++ bashnya-hw2/main.go
@@ -22,7 +22,7 @@ func main() {
 		if err == nil {
 			if num >= 12307 {
 				fmt.Println(input, "не подходит, оно не меньше 12307")
-			} else if num*-1 > 12307 {
+			} else if num*-1 >= 12307 {
 				fmt.Println(input, "не подходит, введите число меньшее по модулю") // иначе там любое отрицательное число можно ввести, вывод бесконечный
 			} else {
 				n = num
@@ -36,19 +36,25 @@ func main() {
 	for n < 12307 {
 		if n < 0 {
 			n *= -1
+			fmt.Println(n)
 		} else if n%7 == 0 {
 			n *= 39
+			fmt.Println(n)
 		} else if n%9 == 0 {
 			n = n*13 + 1
+			fmt.Println(n)
 		} else {
 			n = (n + 2) * 3
+			fmt.Println(n)
 		}
 
 		if n%13 == 0 && n%9 == 0 {
-			fmt.Print("service error")
+			fmt.Println("service error")
+			fmt.Println(n)
 			break
 		} else {
 			n++
+			fmt.Println(n)
 		}
 	}
 
@@ -60,13 +66,18 @@ func main() {
 	digit_tens_th := n / 10000 % 10
 	digit_hndrds_th := n / 100000
 
+	var more999 bool
+	if n > 999 {
+		more999 = true
+	}
+
 	var spelling string
-	spelling += SpellingHundreds(digit_hndrds_th) + " "
-	spelling += SpellingTens(digit_tens_th, digit_units_th) + " "
-	spelling += SpellingUnits(true, digit_hndrds_th, digit_tens_th, digit_units_th) + " "
-	spelling += SpellingWordThousands(true, digit_tens_th, digit_units_th) + " "
-	spelling += SpellingHundreds(digit_hndrds) + " "
-	spelling += SpellingTens(digit_tens, digit_units) + " "
+	spelling += SpellingHundreds(digit_hndrds_th)
+	spelling += SpellingTens(digit_tens_th, digit_units_th)
+	spelling += SpellingUnits(true, digit_hndrds_th, digit_tens_th, digit_units_th)
+	spelling += SpellingWordThousands(more999, digit_tens_th, digit_units_th)
+	spelling += SpellingHundreds(digit_hndrds)
+	spelling += SpellingTens(digit_tens, digit_units)
 	spelling += SpellingUnits(false, digit_hndrds, digit_tens, digit_units)
 
 	fmt.Printf("Итоговое число: %d\nЕго запись: %s", n, spelling)
